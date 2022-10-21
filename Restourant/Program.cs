2f1b0bd3cfa588e76dbd228dc98b84766e28aed0
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2022-10-21 20:05:20 +0300

    LessonHW02

---
+++ Restourant/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+using var rest = new Hall();
+while (true)
+{
+    Console.WriteLine("Привет! Желаете забронировать или освободить столик?\n" +
+                      "1 - забронировать, мы уведомим Вас по смс (асинхронно)\n" +
+                      "2 - забронировать, подождите на линии, мы Вас оповестим (синхронно)\n" +
+                      "3 - освободить, мы уведомим Вас по смс (асинхронно)\n" +
+                      "4 - освободить, подождите на линии, мы Вас оповестим (синхронно)\n"
+                      );
+
+    var choiceValid = int.TryParse(Console.ReadLine(), out var choice);
+    if (!choiceValid || (choiceValid && choice is < 1 or > 4))
+    {
+        Console.WriteLine("Введите, пожалуйста от 1 до 4");
+        continue;
+    }
+
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+
+    switch (choice)
+    {
+        case 1:
+            rest.BookFreeTableAsync(1);
+            break;
+        case 2:
+            rest.BookFreeTable(1);
+            break;
+        case 3:
+        case 4:
+            Console.WriteLine("Укажите номер столика");
+            int.TryParse(Console.ReadLine(), out var tableId);
+            if (choice == 3)
+            {
+                rest.FreeTableAsync(tableId);
+            }
+            else
+            {
+                rest.FreeTable(tableId);
+            }
+            break;
+    }
+
+    Console.WriteLine("Спасибо за Ваше обращение!");
+    stopWatch.Stop();
+    var ts = stopWatch.Elapsed;
+    Console.WriteLine($"{ts.Seconds:80}:{ts.Milliseconds:00}");
+}
