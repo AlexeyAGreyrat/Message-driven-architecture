506965536d1e75e9f69e4727cae615a41e49b596
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2022-10-25 18:28:08 +0300

    Lesson3

---
+++ Messaging/DishReady.cs
@@ -0,0 +1,28 @@
+﻿namespace Messaging;
+
+
+public enum Dish
+{
+    Chicken = 0,
+    Pizza = 1,
+    Pasta = 2,
+    Lasagna = 3
+}
+public interface IDishReady
+{
+    public Guid OrderId { get; }
+        
+    public bool Ready { get; }
+}
+
+public class DishReady : IDishReady
+{
+    public DishReady(Guid orderId, bool ready)
+    {
+        OrderId = orderId;
+        Ready = ready;
+    }
+
+    public Guid OrderId { get; }
+    public bool Ready { get; }
+}
---
+++ Restourant.Notifications/Worker.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Messaging;
+using Microsoft.Extensions.Hosting;
+using System.Text;
+
+namespace Restourant.Notifications
+{
+    public class Worker : BackgroundService
+    {
+        private readonly Consumer _consumer;
+        private readonly IBus _bus;
+        private readonly Hall _hall;
+        public Dish? dish;
+        public Worker(IBus bus,Hall hall)
+        {
+            _bus = bus; 
+            _hall = hall;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000,stoppingToken);
+                Console.WriteLine("Привет! Желаете забронировать столик?");
+                await _bus.Publish(new TableBooked(NewId.NextGuid(), NewId.NextSequentialGuid(), true, dish));
+            }            
+        }
+    }
+}
