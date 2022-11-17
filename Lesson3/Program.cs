64747e9b31d2f275cd81b8729ae043d5a3902993
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2022-11-17 12:33:26 +0300

    Lesson6

---
+++ Lesson3/Program.cs
@@ -0,0 +1,77 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurant.Booking;
+using Restaurant.Booking.Consumers;
+using Restaurant.Booking.Sagas;
+using Restaurant.Messages;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer().AddSwaggerGen();
+
+builder.Services
+    .AddSingleton<Hall>()
+    .AddSingleton<Manager>()
+    .AddSingleton(typeof(IRepository<>), typeof(InMemoryRepository<>));
+
+builder.Services
+    .AddTransient<RestaurantBooking>()
+    .AddTransient<RestaurantBookingSaga>();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<RestaurantBookingRequestConsumer>(cfg =>
+    {
+        cfg.UseMessageRetry(r => r.Incremental(3, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2)));
+        cfg.UseScheduledRedelivery(r => r.Incremental(3, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(10)));
+    })
+        .Endpoint(e => e.Temporary = true);
+    x.AddConsumer<BookingRequestFaultConsumer>(cfg =>
+    {
+        cfg.UseMessageRetry(r => r.Incremental(3, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2)));
+        cfg.UseScheduledRedelivery(r => r.Incremental(3, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(10)));
+    }).Endpoint(e => e.Temporary = true);
+    x.AddConsumer<BookingCancellationConsumer>(cfg =>
+    {
+        cfg.UseMessageRetry(r => r.Incremental(3, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(2)));
+        cfg.UseScheduledRedelivery(r => r.Incremental(3, TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(10)));
+    }).Endpoint(e => e.Temporary = true);
+
+    x.AddSagaStateMachine<RestaurantBookingSaga, RestaurantBooking>()
+        .Endpoint(e => e.Temporary = true)
+        .InMemoryRepository();
+
+    x.AddDelayedMessageScheduler();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("amqps://vavewpmm:g2_Mb_Y4Sj7lhMP9W01CdtM7a70K48xn@beaver.rmq.cloudamqp.com/vavewpmm");
+        cfg.UseDelayedMessageScheduler();
+        cfg.UseInMemoryOutbox();
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+var app = builder.Build();
+
+app.UseSwagger().UseSwaggerUI();
+
+app.MapPost("/Book", static async (int countOfPersons, Dish? dish, [FromServices] Manager manager, [FromServices] IBus messageBus) =>
+{
+    BookingRequest order = new(
+        orderId: NewId.NextGuid(),
+        clientId: NewId.NextSequentialGuid(),
+        preOrder: dish,
+        creationDate: DateTime.UtcNow,
+        incomeTime: Random.Shared.Next(7, 15),
+        countOfPersons: countOfPersons);
+    await messageBus.Publish<IBookingRequest>(order);
+    return Results.Ok(order.OrderId);
+});
+
+app.MapPost("/Free/{orderId}", static (Guid orderId, Manager manager) => Results.Problem("Not released feature"));
+
+app.Run();
